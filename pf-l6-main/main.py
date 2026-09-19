559006a3aa21628f038322291b18a617f6d07ef1
Elizabeth Candelario <albertoelizabeth0427@gmail.com>
2026-09-19 00:58:14 -0600

    renombre el archivo a main.py

--- pf-l6-main/main.py
+++ pf-l6-main/main.py
@@ -1,9 +1,15 @@
+def main():
+  print("Hello learners!")
+
+if __name__=="__main__":
+  main()
+
 def trivia_fetch (num):
     trivias={1: "UNO es un divertido juego de cartas",
             2: "Dos litros de agua al día son necesarios para una buena salud adulta",
             3:  "tres corazones tienen los pulpos" ,
-            4:  "cuatro patas debe tener una silla",
-            5:  "cinco son los continentes que existen"}
+           42:  "42 pulgadas es  la longitud maxima de un bate de baseball",
+          1000:  "Las mil y una noches es un cuento "}
     
     trivia={"number":num, 
                  "text":trivias [num] }
@@ -11,7 +17,7 @@ def trivia_fetch (num):
       
       
 def main():
-    numero = int(input("Ingresa un número del uno al 5: "))
+    numero = int(input("Ingresa un número: "))
     
      
     trivia = trivia_fetch(numero)
@@ -22,7 +28,6 @@ def main():
     print(trivia)
 if __name__=="__main__":
   main()
-
 
 
 
