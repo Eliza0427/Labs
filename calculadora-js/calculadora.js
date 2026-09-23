26ad6a2de693d485bdfa1c1a9cd382aa35f674f6
Elizabeth Candelario <albertoelizabeth0427@gmail.com>
2026-09-22 21:19:43 -0600

    Subo carpeta de calculadora con js

---
+++ calculadora-js/calculadora.js
@@ -0,0 +1,26 @@
+let nombre = prompt("Ingresa tu nombre: ");
+console.log("Hola" + nombre + "! Bienvenido a la calculadora."); 
+
+let numero1=Number(prompt("Ingrese un número:"));
+let numero2=Number(prompt("Ingrese otro número:"));
+console.log("La suma es: " + (numero1 + numero2)); 
+console.log("La resta es: " + (numero1 - numero2));
+console.log("La multiplicación es: " + (numero1 * numero2));
+console.log("La división es: " + (numero1 / numero2));
+
+// Otra forma de hacerlo es guardando los resultados en variables y luego imprimirlos
+let nombre = prompt("Ingresa tu nombre: ");
+console.log("Hola" + nombre + "! Bienvenido a la calculadora."); 
+
+let numero1=Number(prompt("Ingrese un número:"));
+let numero2=Number(prompt("Ingrese otro número:"));
+
+ let suma = numero1 + numero2;
+ let resta = numero1 - numero2;
+ let multiplicacion = numero1 * numero2;
+ let division = numero1 / numero2;
+
+ console.log("La suma es: " + suma);
+ console.log("La resta es: " + resta);
+ console.log("La multiplicación es: " + multiplicacion);
+ console.log("La división es: " + division);  
