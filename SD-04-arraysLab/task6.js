0f86f78e2073c70c265126470dee607abc5d3b20
Elizabeth Candelario <albertoelizabeth0427@gmail.com>
2026-09-25 19:24:19 -0600

    Agrego lab de arrays

---
+++ SD-04-arraysLab/task6.js
@@ -0,0 +1,11 @@
+//6. Use `concat()` to merge the two given arrays, arr1 and arr2, 
+// in order, and then print the new array that's created.
+const arr1 = ["hello"];
+const arr2 = ["world"];
+
+// Type your code below this line!
+arr1.concat(arr2)
+const newarray = arr1.concat(arr2)
+
+// Type your code above this line!
+console.log(newarray);
---
+++ SD-04-arraysLab/task7.js
@@ -0,0 +1,20 @@
+
+const arr = [
+    [0,1,2,3,4,5,6,7,8,9],
+    [10,11,12,13,14,15,16,17,18,19],
+    [20,21,22,23,24,25,26,27,28,29]
+  ]
+  
+  // Type your code below this line!
+  const num =[11,22,33,44,55]
+  arr.splice(1,0,num);
+  
+  // Type your code above this line!
+  console.log(arr);
+  
+
+  /*7. A matrix is an array of arrays, representing a grid with rows and columns. Use this task to experiment with matrices!
+    * Can you add a single number to an existing row? 
+    * Can you add a whole new row of numbers? Si
+    * Can you remove a single number from a single row?
+    * Can you reverse one of the rows without affecting the others*/
